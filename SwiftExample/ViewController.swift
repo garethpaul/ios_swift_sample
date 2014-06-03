52a8005e577fe545acbb16de5fc7051ca4616b43
Gareth Paul Jones <gareth@garethpaul.com>
2014-06-03 14:34:09 -0700

    adding screenshot

--- SwiftExample/ViewController.swift
+++ SwiftExample/ViewController.swift
@@ -1,25 +1,69 @@
 //
 //  ViewController.swift
-//  SwiftExample
 //
-//  Created by Gareth Paul Jones on 6/3/14.
-//  Copyright (c) 2014 Gareth Paul Jones. All rights reserved.
 //
 
 import UIKit
+import QuartzCore
 
-class ViewController: UIViewController {
-                            
+class SearchResultsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, APIControllerProtocol {
+    
+    var api: APIController = APIController()
+    @IBOutlet var appsTableView : UITableView
+    var tableData: NSArray = NSArray()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        api.delegate = self
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+        api.searchItunesFor("Angry Birds");
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
     }
+    
+    
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
+        
+        var rowData: NSDictionary = self.tableData[indexPath.row] as NSDictionary
+        
+        cell.text = rowData["trackName"] as String
+        
+        // Grab the artworkUrl60 key to get an image URL for the app's thumbnail
+        var urlString: NSString = rowData["artworkUrl60"] as NSString
+        var imgURL: NSURL = NSURL(string: urlString)
+        
+        // Download an NSData representation of the image at the URL
+        var imgData: NSData = NSData(contentsOfURL: imgURL)
+        cell.image = UIImage(data: imgData)
 
-
+        // Circular image
+        cell.imageView.layer.cornerRadius = 23
+        cell.imageView.layer.masksToBounds = true
+        cell.imageView.layer.borderWidth = 0
+        
+        
+        // Get the formatted price string for display in the subtitle
+        //var formattedPrice: NSString = rowData["formattedPrice"] as NSString
+        
+        //cell.detailTextLabel.text = formattedPrice
+        
+        return cell
+    }
+    
+    func didRecieveAPIResults(results: NSDictionary) {
+        // Store the results in our table data array
+        if results.count>0 {
+            self.tableData = results["results"] as NSArray
+            self.appsTableView.reloadData()
+            UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+        }
+    }
+    
 }
+
+
+
 
